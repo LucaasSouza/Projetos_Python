216eaf5051e2a2e8c1f8bd3ba036f0b9518b3632
LucasSouza <82621071+LucaasSouza@users.noreply.github.com>
2024-04-17 14:27:32 -0300

    Adição dos comentários

--- forca-game.py
+++ forca-game.py
@@ -1,21 +1,24 @@
-message = 'Hello World'
-placeHolder = ''
-tentativas = 6
+message = 'Hello World' # Mensagem que vai ser descoberta
+placeHolder = '' # Faz o replace dos caracteres da string acima para '_'
+tentativas = 6 # Quantidade de tentativas que o jogador vai ter
 
+# Faz o replace dos caracteres da string acima para '_'
 for m in message:
     placeHolder += '_' if m != ' ' else ' '
 
+# Loop do jogo
 while tentativas > 0 and placeHolder != message:
     print('Digite uma letra: ')
     letter = input()
-    errou = True
+    errou = True # Flag que verifica se o caractere inserido pelo jogador foi correto
 
     for i in range(len(message)):
-        if message[i] == letter:
+        if message[i] == letter: # Verifica se o caractere inserido pelo jogador foi correto
             errou = False
             placeHolder = placeHolder[:i] + letter + placeHolder[i + 1:]
             print(placeHolder)
 
+    # Caso o caractere inserido pelo jogador seja errado 
     if errou:
         tentativas -= 1
         errou = True
